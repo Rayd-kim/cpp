f97a592031425ad0cc4ea334e2cf76161c32abd3
Yousung Kim <youskim@c10r5s14.42seoul.kr>
2022-08-05 19:51:37 +0900

    0805

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -12,25 +12,42 @@ Fixed::Fixed(const Fixed &fixed)
 	*this = fixed;
 }
 
+Fixed::Fixed(const int param)
+{
+	std::cout << "Int constructor called" << std::endl;
+	fixed_num = param * (1 << fract_bit);
+}
+
+Fixed::Fixed(const float num)
+{
+	std::cout << "Float constructor called" << std::endl;
+	fixed_num = roundf(num * (1 << fract_bit));
+}
+
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
 }
 
+float	Fixed::toFloat(void) const
+{
+	return (float(fixed_num) / (1 << 8));
+}
+
+int		Fixed::toInt(void) const
+{
+	return (fixed_num >> 8);
+}
+
 Fixed&	Fixed::operator=(const Fixed &fixed)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
-	fixed_num = fixed.getRawBits();
+	fixed_num = fixed.fixed_num;
 	return (*this);
 }
 
-int		Fixed::getRawBits(void) const
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed)
 {
-	std::cout << "getRawBits member function called" << std::endl;
-	return (fixed_num);
-}
-
-void	Fixed::setRawBits(int const raw)
-{
-	fixed_num = raw;
+	os << fixed.toFloat();
+	return os;
 }
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -2,6 +2,7 @@
 # define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
@@ -11,11 +12,17 @@ class Fixed
 
 	public:
 		Fixed();
+		Fixed(const int	param);
 		Fixed(const Fixed &fixed);
+		Fixed(const float num);
 		~Fixed();
+
 		Fixed&	operator=(const Fixed &fixed);
-		int		getRawBits(void) const;
-		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
 
 # endif
