bcba7d03d57a88685e7c70e9e6240f1b8b2a7ca9
Yousung Kim <youskim@c6r9s4.42seoul.kr>
2022-09-21 15:38:15 +0900

    0921

--- cpp06/ex00/Conversion.cpp
+++ cpp06/ex00/Conversion.cpp
@@ -4,38 +4,28 @@ void	conversion(std::string arg)
 {
 	if( (arg[0] > 32 && arg[0] < 127) && !(arg[0] >= '0' && arg[0] <= '9') && arg.length() == 1)
 		conversion_c(arg);
-	else if ((static_cast<int>(arg.find('f')) != -1 && static_cast<int>(arg.find('.')) != -1) \
-			|| !arg.compare("-inff") || !arg.compare("+inff") || !arg.compare("inff")|| !arg.compare("nanf"))
+	else if (static_cast<int>(arg.find('f')) != -1 || \
+			(static_cast<int>(arg.find('f')) != -1 && static_cast<int>(arg.find('.')) != -1) || check_inf_nan(arg) == FLO)
 		conversion_f(arg);
-	else if (!arg.compare("-inf") || !arg.compare("+inf") || !arg.compare("inf") ||\
-			 !arg.compare("nan") || static_cast<int>(arg.find('.')) != -1)
+	else if (check_inf_nan(arg) == DOU || static_cast<int>(arg.find('.')) != -1)
 		conversion_d(arg);
 	else
 		conversion_i(arg);
 }
 
-
 void	conversion_c(std::string arg)
 {
-	try
-	{
-		char c = arg[0];
-		std::cout << "char: ";
-		std::cout << "\'" << c << "\'" << std::endl;
-		int	i = static_cast<int>(c);
-		float f = static_cast<float>(c);
-		double d = static_cast<double>(c);
+	char c = arg[0];
+	std::cout << "char: ";
+	std::cout << "\'" << c << "\'" << std::endl;
+	int	i = static_cast<int>(c);
+	float f = static_cast<float>(c);
+	double d = static_cast<double>(c);
 
-		std::cout << "int: " << i << std::endl;
-		std::cout << "float: " << f << "f" << std::endl;
-		std::cout << "double: " << d << std::endl;
-	}
-	catch (...)
-	{
-		std::cout << "int: conversion impossible" << std::endl;
-		std::cout << "float: conversion impossible" << std::endl;
-		std::cout << "double: conversion impossible" << std::endl;
-	}
+	std::cout << "int: " << i << std::endl;
+	std::cout << "float: " << f << "f" << std::endl;
+	std::cout << "double: " << d << std::endl;
+
 }
 
 void	conversion_i(std::string arg)
@@ -76,7 +66,15 @@ void	conversion_f(std::string arg)
 		unsigned char c = static_cast<unsigned char>(f);
 		double d = static_cast<double>(f);
 
-		if (i < 0 || i > 127)
+		if (check_inf_nan(arg) == FLO)
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: " << f << "f" << std::endl;
+			std::cout << "double: " << d << std::endl;
+			return ;
+		}
+		else if (i < 0 || i > 127)
 			std::cout << "char: " << "impossible" << std::endl;
 		else if (c < 33 || c > 126)
 			std::cout << "char: " << "Not displayable" << std::endl;
@@ -104,7 +102,15 @@ void	conversion_d(std::string arg)
 		unsigned char c = static_cast<unsigned char>(d);
 		float f = static_cast<float>(d);
 
-		if (i < 0 || i > 127)
+		if (check_inf_nan(arg) == DOU)
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: " << f << "f" << std::endl;
+			std::cout << "double: " << d << std::endl;
+			return ;
+		}
+		else if (i < 0 || i > 127)
 			std::cout << "char: " << "impossible" << std::endl;
 		else if (c < 33 || c > 126)
 			std::cout << "char: " << "Not displayable" << std::endl;
@@ -121,4 +127,14 @@ void	conversion_d(std::string arg)
 		std::cout << "float: conversion impossible" << std::endl;
 		std::cout << "double: conversion impossible" << std::endl;
 	}
+}
+
+int	check_inf_nan(std::string arg)
+{
+	if (!arg.compare("-inff") || !arg.compare("+inff") || !arg.compare("nanf") || !arg.compare("inff"))
+		return (FLO);
+	else if (!arg.compare("-inf") || !arg.compare("+inf") || !arg.compare("inf") || !arg.compare("nan"))
+		return (DOU);
+	else
+		return (0);
 }
--- cpp06/ex00/Conversion.hpp
+++ cpp06/ex00/Conversion.hpp
@@ -4,10 +4,14 @@
 #include <iostream>
 #include <string>
 
+#define FLO 1
+#define DOU 2
+
 void	conversion(std::string arg);
 void	conversion_i(std::string arg);
 void	conversion_c(std::string arg);
 void	conversion_f(std::string arg);
 void	conversion_d(std::string arg);
+int		check_inf_nan(std::string arg);
 
 #endif
