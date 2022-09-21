c62deee3f8d3a22722a7f0a08b4f35c368c49aa2
Yousung Kim <youskim@c6r9s4.42seoul.kr>
2022-09-21 16:29:33 +0900

    0921

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: youskim <youskim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 16:01:38 by youskim           #+#    #+#             */
-/*   Updated: 2022/09/19 20:17:58 by youskim          ###   ########.fr       */
+/*   Updated: 2022/09/21 16:29:05 by youskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,65 +34,5 @@ class	Array
 		T*	getArray(void);
 		T&	operator[](int i);
 };
-
-template <class T>
-Array<T>::Array()
-{
-	arr = NULL;
-	len = 0;
-}
-
-template <class T>
-Array<T>::Array(unsigned int n) : len(n)
-{
-	arr = new T[n];
-}
-
-template <typename T>
-unsigned int	Array<T>::size(void) const
-{
-	return (len);
-}
-
-template <typename T>
-T*	Array<T>::getArray(void)
-{
-	return (arr);
-}
-
-template <typename T>
-T&	Array<T>::operator[](int i)
-{
-	if (i < 0 || i >= len)
-		throw(std::exception());
-	else
-		return (arr[i]);
-}
-
-template <typename T>
-Array<T>&	Array<T>::operator=(Array<T> &a)
-{
-	delete [] arr;
-	len = a.size();
-	arr = new T[len];
-	for (int i = 0; i < len; i++)
-		arr[i] = a.getArray()[i];
-	return (*this);
-}
-
-template <typename T>
-Array<T>::Array(Array<T> &a)
-{
-	len = a.size();
-	arr = new T[len];
-	for (int i = 0; i < len; i++)
-		arr[i] = a.getArray()[i];
-}
-
-template <class T>
-Array<T>::~Array()
-{
-	delete [] arr;
-}
 
 # endif
